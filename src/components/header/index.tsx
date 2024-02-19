d7df49caa14f7d166a713275f6d73b5125bab653
JodieAddis <jodie.addis.92@gmail.com>
2024-02-19 11:54:22 +0100

    Creation header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import Logo from "../logo";
+import NavBar from "../navbar";
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <Logo />
+      <NavBar />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/logo/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo: React.FC = () => {
+  return (
+    <>
+      <h1 className="text-red">crowdfund</h1>
+    </>
+  );
+};
+
+export default Logo;
