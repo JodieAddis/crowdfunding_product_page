56a79e5cf1d108c124ff3d4b8a45a111fb2530b0
JodieAddis <jodie.addis.92@gmail.com>
2024-02-22 15:54:10 +0100

    Creation of close and hamburger icon component + adding icons menu + switch icons with button.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,15 +1,42 @@
 import React from "react";
+import { useState } from "react";
+import IconHamburger from "../icons/IconHamburger";
+import IconCloseMenu from "../icons/IconCloseMenu";
 
 const Component: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex flex-row mt-14 mr-24">
-      <ul className="flex flex-row text-sm capitalize font-bold">
-        <li className="mx-3 cursor-pointer">about</li>
-        <li className="mx-3 cursor-pointer">discover</li>
-        <li className="mx-3 cursor-pointer">get started</li>
-      </ul>
+      {isOpen ? (
+        <button
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          <IconCloseMenu />
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          <IconHamburger />
+        </button>
+      )}
     </div>
   );
 };
 
 export default Component;
+
+{
+  /* 
+DESKTOP: 
+<ul className="flex flex-row text-sm capitalize font-bold">
+<li className="mx-3 cursor-pointer">about</li>
+<li className="mx-3 cursor-pointer">discover</li>
+<li className="mx-3 cursor-pointer">get started</li>
+</ul> */
+}
