fa797ad466b49615e7e94c547fe42ce68f8d4b60
JodieAddis <jodie.addis.92@gmail.com>
2024-02-27 14:58:44 +0100

    First attempt to apply custom hook for responsive button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
-import useScreenSize from "./hook/useScreenSize";
 
 function App() {
-  // const width = window.innerWidth;
-  // const [currentSize, setCurrentSize] = useState<number>(width);
-  //   const currentSize = useTonCustomHook()
-  // {currentSize ? bookmark mobile : bookmark pc}
-
-  const screenSize = useScreenSize();
-
   return (
     <>
       <div id="overlay" className="z-10 bg-black">
         <Header />
-        <div>
-          <h1 className={`text-${screenSize ? "white" : "surfieGreen"}`}>
-            TEST
-          </h1>
-        </div>
         <MainContent />
       </div>
     </>
--- src/hook/useScreenSize.tsx
+++ src/hook/useScreenSize.tsx
@@ -4,17 +4,14 @@ const useScreenSize = () => {
   const [isCurrentSize, setIsCurrentSize] = useState<boolean>(
     window.innerWidth < 1440 ? true : false,
   );
-  //Initialement, la taille de l'écran est celle sur lequel on est.
+
   useEffect(() => {
     const handleResize = () => {
       setIsCurrentSize(window.innerWidth < 1440 ? true : false);
     };
     window.addEventListener("resize", handleResize);
-    //Ecouteur d'évènement 'resize' qui permet de détecter le changement de taille de l'écran. Quand l'écran change, mise à jour de la taille avec l'exécution de  'handleResize'.
 
-    //Clean up :
     return () => {
-      //Cleanup de l'écouteur d'évènement
       window.removeEventListener("resize", handleResize);
     };
   }, []);
